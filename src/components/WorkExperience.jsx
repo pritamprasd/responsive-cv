b07beb90016235cba27f80db8f46f879f115a11c
Pritam Prasad <5097255+pritamprasd@users.noreply.github.com>
2025-03-14 18:12:29 +0530

    fix

--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -25,7 +25,7 @@ function Company({company}){
                 </div>
             </div>
             <ul className='company-content'>
-                {company['desc'].map(d => <li dangerouslySetInnerHTML={{ __html: d }}key={d}/>)}
+                {company['desc'].map(d => <li dangerouslySetInnerHTML={{ __html: d }} key={d}/>)}
             </ul>            
         </div>
     );
