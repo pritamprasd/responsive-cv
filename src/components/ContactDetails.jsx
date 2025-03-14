31c316c2ed62c4061b27a8a14cb7fb2f644c8abf
Pritam Prasad <5097255+pritamprasd@users.noreply.github.com>
2025-03-14 20:29:31 +0530

    linkedin

--- src/components/ContactDetails.jsx
+++ src/components/ContactDetails.jsx
@@ -16,13 +16,7 @@ export default function ContactDetails({details}) {
                     </svg> <a href={'https://github.com/'+ details['github']}>{details['github']}</a>
                 </div>
                 <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                        stroke="black" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round"
-                        className="feather feather-linkedin">
-                        <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
-                        <rect x="2" y="9" width="4" height="12"></rect>
-                        <circle cx="4" cy="4" r="2"></circle>
-                    </svg> <a href={'https://www.linkedin.com/in/' + details['linkedin']}>{details['linkedin']}</a>
+                    <a href={'https://www.' + details['linkedin']}>{details['linkedin']}</a>
                 </div>
             </div>
             <hr className='line'/>
